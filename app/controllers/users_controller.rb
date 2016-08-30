2705dc067c84798d7e1123ffc69304303177972a
Your Name <eric.joseph.salazar@gmail.com>
2016-08-30 09:58:20 -0700

    Backend Auth | not fully implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def new
+    render json: User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(user)
+      render json: @user
+    else
+      flash.new[:errors] = @user.errors.full_messages
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,39 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true, uniqueness: true
+  validates :email, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  attr_reader :password
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user && user.valid_password?(password)
+    user
+  end
+
+  def reset_session_token!
+    self.session_token = generate_token
+    save!
+    session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= generate_token
+  end
+
+  def generate_token
+    SecureRandom.urlsafe_base64(32)
+  end
 end
