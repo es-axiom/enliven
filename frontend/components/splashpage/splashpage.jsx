1b9c75be0ea1c0d96e9a107f62a3430d9766b59d
Your Name <eric.joseph.salazar@gmail.com>
2016-08-31 15:29:58 -0700

    Fixed some styling

--- frontend/components/splashpage/splashpage.jsx
+++ frontend/components/splashpage/splashpage.jsx
@@ -7,6 +7,9 @@ class Splashpage extends React.Component {
         <div className="Overlay-quote">
           <span className="Overlay-quote-text">Stick with your team anywhere you go.</span>
         </div>
+        <div className="Overlay-Team-Finder">
+          <h3>Find Your Team</h3>
+        </div>
       </div>
     )
   }
