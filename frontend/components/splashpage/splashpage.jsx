d2ac365d327faca69000fef594a189fe30889b7d
Your Name <eric.joseph.salazar@gmail.com>
2016-08-31 10:44:28 -0700

    Serve img in https

--- frontend/components/splashpage/splashpage.jsx
+++ frontend/components/splashpage/splashpage.jsx
@@ -5,7 +5,7 @@ class Splashpage extends React.Component {
     return (
       <div className="Overlay">
         <div className="Overlay-quote">
-          <img className="home_image" src="http://res.cloudinary.com/es-axiom/image/upload/v1472661914/rolling-hills_ahfnsi.jpg" />
+          <img className="home_image" src="https://res.cloudinary.com/es-axiom/image/upload/v1472661914/rolling-hills_ahfnsi.jpg" />
           <span className="Overlay-quote-text">Stick with your team anywhere you go.</span>
         </div>
       </div>
