256d6f9f50ccbaefa8e6b0ef2bd0b989189a0128
Eric Salazar <eric.joseph.salazar@gmail.com>
2016-10-26 23:46:37 -0700

    Working on styling

--- frontend/components/navbar/navbar_static.jsx
+++ frontend/components/navbar/navbar_static.jsx
@@ -4,12 +4,6 @@ import { Link } from 'react-router';
     //TODO: Modal for About
 const NavBarStatic = () => (
   <ul className='header-static'>
-    <li className='header-static-logo'>
-      <Link to='/home'>
-        <img className='logo-img'
-          src='https://res.cloudinary.com/es-axiom/image/upload/c_scale,w_72/v1473569048/06d53573b62aedaa4d410aa943c1288b_vvgwij.jpg' />
-      </Link>
-    </li>
     <li className='header-static-title'>
       <Link to='/home' className='header-title'><h1 className='header-static-link'>Enliven</h1></Link>
     </li>
