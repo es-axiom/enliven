55997be83214f7d8fe14fd60ffaa7fc25cc8d1a5
es-axiom <eric.joseph.salazar@gmail.com>
2016-10-29 19:09:36 -0700

    Updating styling - splashpage

--- frontend/components/splashpage/splashpage.jsx
+++ frontend/components/splashpage/splashpage.jsx
@@ -15,13 +15,15 @@ class Splashpage extends React.Component {
   render() {
     return (
       <div className="Overlay">
+        <ul className="Overlay-list">
+          <li><Link to='/home'>Home</Link></li>
+          <li><Link to='/login'>Login</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/contact'>Contact</Link></li>
+        </ul>
         <div className="Overlay-quote">
           <span className="Overlay-quote-text">Stick with your team anywhere you go.</span>
         </div>
-        <ul className="Overlay-list">
-          <li><Link to='/home'>Home</Link></li>
-          <li><Link to='/login'>{this.secondButton()}</Link></li>
-        </ul>
       </div>
     )
   }
